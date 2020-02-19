e58069abdc8237caef7d4152a2896171c90c809d
MaxVGC <carlos.andres12390@live.com>
2020-02-18 19:29:08 -0500

    Error css

--- view/Js/Main_js.js
+++ view/Js/Main_js.js
@@ -2,11 +2,11 @@
 const btnToggle = document.querySelector('.toggle-btn');
 
 btnToggle.addEventListener('click', function () {
-  console.log('clik')
+  console.log('clik');
   document.getElementById('sidebar').classList.toggle('active');
-  console.log(document.getElementById('sidebar'))
+  console.log(document.getElementById('sidebar'));
   document.getElementById('navbar2').classList.toggle('active');
-  console.log(document.getElementById('sidebar'))
+  console.log(document.getElementById('navbar2'));
   document.getElementById('div_prueba').classList.toggle('active');
-  console.log(document.getElementById('sidebar'))
+  console.log(document.getElementById('sidebar'));
 });
